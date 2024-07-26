53137a8063394b0b287f0750070a424dbdca8df7
Sitt Guruvanich <aekazitt@gmail.com>
2024-07-27 00:18:58 +0700

    Add prompt to next step

--- src/App.vue
+++ src/App.vue
@@ -135,6 +135,28 @@ let openGithubRepository = () => {
             </CardFooter>
           </Card>
         </Transition>
+        <Transition name="fade">
+          <Card v-if="address">
+            <CardHeader>
+              <CardTitle> Statechain </CardTitle>
+              <CardDescription>
+                Statechains allow users to rapidly and cheaply transfer real bitcoins between each other without relying on a network of payment channels.
+                Double-spending prevention is done by
+                <italic>
+                  semi-trusted
+                </italic>
+                Statechain operators. One may try to steal funds from you by sharing their vTXO while obscuring ties with the Statechain operator, or even
+                be one him/herself. Due to reduced security when compared to Bitcoin, Statechains are able to achieve many feats and extensibilities which
+                makes the risk and reward of using one for fun or profit open to personal discretion. May the odds be ever in your favour.
+              </CardDescription>
+            </CardHeader>
+            <CardFooter class="justify-end">
+              <Button>
+                Create Disposable Statechain
+              </Button>
+            </CardFooter>
+          </Card>
+        </Transition>
       </div>
     </section>
   </div>
