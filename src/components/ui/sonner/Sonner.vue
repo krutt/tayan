4bf4109a87ac14ff526f36d1a61a6e8cea75cb8b
Sitt Guruvanich <aekazitt@gmail.com>
2024-07-26 19:09:53 +0700

    Add Dialog and Sonner Shadcn-vue components

---
+++ src/components/ui/sonner/Sonner.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { Toaster as Sonner } from 'vue-sonner'
+
+const props = defineProps({
+  invert: { type: Boolean, required: false },
+  theme: { type: String, required: false },
+  position: { type: String, required: false },
+  hotkey: { type: Array, required: false },
+  richColors: { type: Boolean, required: false },
+  expand: { type: Boolean, required: false },
+  duration: { type: Number, required: false },
+  gap: { type: Number, required: false },
+  visibleToasts: { type: Number, required: false },
+  closeButton: { type: Boolean, required: false },
+  toastOptions: { type: Object, required: false },
+  class: { type: String, required: false },
+  style: { type: null, required: false },
+  offset: { type: [String, Number], required: false },
+  dir: { type: String, required: false },
+  icons: { type: Object, required: false },
+  containerAriaLabel: { type: String, required: false },
+  pauseWhenPageIsHidden: { type: Boolean, required: false },
+  cn: { type: Function, required: false },
+})
+</script>
+
+<template>
+  <Sonner
+    class="toaster group"
+    v-bind="props"
+    :toast-options="{
+      classes: {
+        toast:
+          'group toast group-[.toaster]:bg-white group-[.toaster]:text-slate-950 group-[.toaster]:border-slate-200 group-[.toaster]:shadow-lg dark:group-[.toaster]:bg-slate-950 dark:group-[.toaster]:text-slate-50 dark:group-[.toaster]:border-slate-800',
+        description: 'group-[.toast]:text-slate-500 dark:group-[.toast]:text-slate-400',
+        actionButton:
+          'group-[.toast]:bg-slate-900 group-[.toast]:text-slate-50 dark:group-[.toast]:bg-slate-50 dark:group-[.toast]:text-slate-900',
+        cancelButton:
+          'group-[.toast]:bg-slate-100 group-[.toast]:text-slate-500 dark:group-[.toast]:bg-slate-800 dark:group-[.toast]:text-slate-400',
+      },
+    }"
+  />
+</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,7 @@ const animate = require("tailwindcss-animate")
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
-  content: [
-    './src/**/*.{js,jsx,vue}',
-	],
+  content: ['./src/**/*.{js,jsx,vue}'],
   prefix: "",
   theme: {
     container: {
