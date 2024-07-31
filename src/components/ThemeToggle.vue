f3f4bc109978564e5d3f728c32e8886a24bb819e
Sitt Guruvanich <aekazitt@gmail.com>
2024-07-31 14:26:30 +0700

    Remove Dropdown impl; Make ThemeToggle a toggle

--- src/components/ThemeToggle.vue
+++ src/components/ThemeToggle.vue
@@ -1,37 +1,25 @@
 <script setup>
 /* imports */
 import { Button } from '@/components/ui/button'
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu'
 import { Moon, Sun } from 'lucide-vue-next'
 import { useColorMode } from '@vueuse/core'
 
 /* composables */
 let theme = useColorMode({ disableTransition: true })
+
+/* functions */
+let toggleTheme = () => (theme.value = theme.value !== 'dark' ? 'dark' : 'light')
 </script>
 <template>
   <div>
-    <DropdownMenu>
-      <DropdownMenuTrigger as-child>
-        <Button variant="outline" size="icon">
-          <div v-if="theme == 'light'">
-            <Moon />
-          </div>
-          <div v-else>
-            <Sun class="dark:text-white" />
-          </div>
-          <span class="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem @click="theme = 'light'"> Light </DropdownMenuItem>
-        <DropdownMenuItem @click="theme = 'dark'"> Dark </DropdownMenuItem>
-        <DropdownMenuItem @click="theme = 'system'"> System </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <Button @click="toggleTheme" variant="outline" size="icon">
+      <div v-if="theme == 'light'">
+        <Moon />
+      </div>
+      <div v-else>
+        <Sun class="dark:text-white" />
+      </div>
+      <span class="sr-only">Toggle theme</span>
+    </Button>
   </div>
 </template>
